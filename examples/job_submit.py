dbdc99481911aa21b5ee239359027ca8f0072ebe
Zilles <azilles@users.noreply.github.com>
2017-12-20 15:04:13 +0100

    script creates bash files to start the jobs + a script to start all the jobs at once

---
+++ examples/job_submit.py
@@ -0,0 +1,69 @@
+''' This scripts produces the bash files to submit several jobs reading in json files in a specific folder -> e.g. flat100x100_job1.sh
+    In addition a script containing all names of that sripts and submitting them is products (eg. msub flat100x100_job1.sh), called run_RM.sh
+    Start the submitting with ./run_RM.sh
+    
+    -> python job_submit.py
+'''
+
+ #MOAB commands            brief explanation
+ #--------------------------------------------------------------------------
+ #msub                     submits a job and queues it in an input queue.
+ #showq                    displays information about active, eligible,
+                          #blocked, and/or recently completed jobs.
+ #showstart                returns start time of submitted job or resources.
+ #checkjob                 displays detailed job state information.
+ #mjobctl -c <jobid>                   cancels the job `jobid`.
+
+
+import os
+
+### path to where one finds the json files which shall be used
+path_json= '/project/fh1-project-huepra/le6232/data/retro/flat-100x100/fb4e031/'
+
+# path to the scripts for radiomorphing
+path_script='/project/fh1-project-huepra/qc8087/soft/radiomorphing/examples/'
+# python ${path_script}/example_ForHLR.py ${path_json}/events.29939363.1.json 
+
+
+#get an array with all json files which are in the folder
+json_file = [f for f in os.listdir(path_json) if f.endswith('.json')]
+#print json_file
+
+## craeting a bash file containing all the submission commands
+nname='run_RM.sh'
+nfile= open(nname, 'w')
+nfile.write('#!/bin/bash\n')
+
+# producing bash files to run on multiple cores
+for i in range(0, len(json_file )):
+
+        fname='flat100x100_job'+str(i)+'.sh' # filename of each job
+        
+        command='msub '+str(fname)+'\n'
+        nfile.write(command)
+        
+        
+        file= open(fname, 'w')
+        file.write('#!/bin/bash\n')
+        file.write('#\n')
+        file.write('#---------------------\n')
+        file.write('# Scheduler options\n')
+        file.write('#---------------------\n')
+        file.write('#MSUB -q develop\n')
+        file.write('#MSUB -l nodes=1:ppn=1\n')
+        file.write('#MSUB -l walltime=03:00:00\n')
+        file.write('#MSUB -l pmem=1gb\n')
+        file.write('#MSUB -m bea\n')
+        file.write('#MSUB -M zilles@iap.fr\n')
+        file.write('#--------------------- \n')       
+        file.write('\n')
+        file.write('cd $TMP\n')
+        message='echo "Job $MOAB_JOBID runnung in $PWD, started: '  +str(path_script)+'/example_ForHLR.py '+str(path_json)+'/'+str(json_file[i])+'"\n'
+        file.write(message)
+        script='python '+str(path_script)+'/example_ForHLR.py '+str(path_json)+'/'+str(json_file[i])
+        file.write(script)
+        file.close()
+        
+        i=i+1
+        
+nfile.close()
