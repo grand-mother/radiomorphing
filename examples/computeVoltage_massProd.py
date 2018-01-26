c09346d15d92b13e7a758d541c5bb6230d393949
Zilles <azilles@users.noreply.github.com>
2018-01-26 19:08:37 +0100

    removed not needed 'import modules'

--- examples/computeVoltage_massProd.py
+++ examples/computeVoltage_massProd.py
@@ -18,7 +18,7 @@ root_dir = realpath(join(split(__file__)[0], "..")) # = $PROJECT
 sys.path.append(join(root_dir, "lib", "python"))
 import retro
 from retro.event import EventIterator, EventLogger
-import modules
+#import modules
 
 EARTH_RADIUS=6370949. #m
 azstep=5 #step in azimuth in npy file
