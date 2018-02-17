f70ec0949de9dcec306c779f04933f2e625a0a63
Zilles <azilles@users.noreply.github.com>
2018-02-17 15:24:49 +0100

    -

--- examples/job_submit_cv_hack.py
+++ examples/job_submit_cv_hack.py
@@ -21,8 +21,8 @@ run="hotspot-150x67km2"
 hack=1 # off alpha, beta from json file, if =1: alpha=0, beta=0
 
 ### path to where one finds the json files which shall be used
-#path_json= '/project/fh1-project-huepra/le6232/data/retro/'+run+'/seeds/'
-path_json= '/project/fh1-project-huepra/qc8087/radiomorphing/examples/seeds/'+run+'/taus/'
+path_json= '/project/fh1-project-huepra/le6232/data/retro/'+run+'/taus/'
+#path_json= '/project/fh1-project-huepra/qc8087/radiomorphing/examples/seeds/'+run+'/taus/'
 
 
 # path to the scripts for radiomorphing
@@ -35,14 +35,14 @@ json_file = [f for f in os.listdir(path_json) if f.endswith('.json')]
 #print json_file
 
 ## craeting a bash file containing all the submission commands
-nname='run_CV_hack.sh'
+nname='/project/fh1-project-huepra/qc8087/run_CV_hack.sh'
 nfile= open(nname, 'w')
 nfile.write('#!/bin/bash\n')
 
 # producing bash files to run on multiple cores
 for i in range(0, len(json_file )):
 
-        fname=run+'_cv_hack_job'+str(i)+'.sh' # filename of each job
+        fname="/project/fh1-project-huepra/qc8087/"+run+'_cv_hack_job'+str(i)+'.sh' # filename of each job
         
         command='msub '+str(fname)+'\n'
         nfile.write(command)
