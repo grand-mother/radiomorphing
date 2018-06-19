bug fix: for scaling factors and refercative index use altitude instead of injection height

lib/python/radiomorphing/core.py
@@ -35,7 +35,7 @@ def _ProjectPointOnPlane(a,b,d, p):
     point= p+ t*n
     return point
 
-def interpolate(path0, path1, path2, zenith=None, azimuth=None,injection_height=None, scaled=True):
+def interpolate(path0, path1, path2, zenith=None, azimuth=None,injection_height=None, altitude=None, scaled=True):
     """Interpolate all traces from the (rescaled) closest neighbours
 
     Args:
@@ -748,7 +748,7 @@ def interpolate(path0, path1, path2, zenith=None, azimuth=None,injection_height=
 
 
             #from utils import get_integratedn(zen2, height_Xmax, height_ant)
-            decay=np.array([0,0, injection_height])
+            decay=np.array([0,0, injection_height]) # in calling script already desired antenna positions changed to refer to (0,0, injection_height of particle)
             
             dist_decay_Xmax= mag(decay-Xmax_pos)
             dist_Xmax_ant=mag(Xmax_pos-positions[b])
@@ -756,7 +756,7 @@ def interpolate(path0, path1, path2, zenith=None, azimuth=None,injection_height=
             # light velocity
             c=299792458*1.e-9 #m/ns
             
-            n=get_integratedn(zen, injection_height, positions[b])
+            n=get_integratedn(zen, injection_height, positions[b])# injection_height needed not altitude
             
             newtime= dist_decay_Xmax/c + dist_Xmax_ant/c*n
             
@@ -807,4 +807,4 @@ def process(sim_dir, shower, antennas, out_dir):
     scale(sim_dir, **shower)
 
     # interpolate the traces.
-    interpolate(antennas, sim_dir, out_dir, shower["zenith"], shower["azimuth"], shower["injection_height"])
+    interpolate(antennas, sim_dir, out_dir, shower["zenith"], shower["azimuth"], shower["injection_height"],shower["altitude"])

lib/python/radiomorphing/scaling.py
@@ -31,7 +31,7 @@ def _getXmax(primarytype, energy, zen2):
 
     return Xmax#/abs(np.cos(np.pi-zen2)) 
 
-def _dist_decay_Xmax(zen2, injh2, Xmax_primary): #zen2: zenith of target shower
+def _dist_decay_Xmax(zen2, altitude, Xmax_primary): #zen2: zenith of target shower, altitude counts for xmax position
     #% Using isothermal Model
     rho_0 = 1.225*0.001#; % kg/m3 to 0.001g/cm3: 1g/cm3=1000kg/m3, since X given in g/cm2
     M = 0.028966#;  %kg/mol - 1000g/mol
@@ -39,7 +39,7 @@ def _dist_decay_Xmax(zen2, injh2, Xmax_primary): #zen2: zenith of target shower
     T = 288.#; % K
     R = 8.32#; J/K/mol , J=kg m2/s2
 
-    hD=injh2
+    hD=altitude
     Xmax_primary= Xmax_primary#* 10. # g/cm2 to kg/m2: 1g/cm2 = 10kg/m2
     gamma=np.pi-zen2 # counterpart of where it goes to
     Re= 6370949 # m, Earth radius
@@ -83,7 +83,7 @@ def _scalingfactors(E1, az1, zen1, injh1, E2, az2, zen2, injh2, phigeo,thetageo,
     #%############## Height+Zenith, distance injection point to xmax rougly 8000m
     primary1='electron'
     Xmax_primary1 = _getXmax(primary1, E1, zen1)# approximation based on values from plots for gamma (=e) and protons (=pi) # g/cm2
-    Xmax_height1, Xmax_distance1 = _dist_decay_Xmax(zen1, injh1, Xmax_primary1)
+    Xmax_height1, Xmax_distance1 = _dist_decay_Xmax(zen1, injh1, Xmax_primary1)# injh1=altitude for reference shower
 
     #hx_ref = h_ref+Xmax_hor1*np.tan(0.5*np.pi-zen1) #   % Height at reference shower Xmax
     hx_ref = h_ref+Xmax_distance1*np.sin(0.5*np.pi-zen1) #   % Height at reference shower Xmax
@@ -92,7 +92,7 @@ def _scalingfactors(E1, az1, zen1, injh1, E2, az2, zen2, injh2, phigeo,thetageo,
 
 
     Xmax_primary2 = _getXmax(primary, E2, zen2)# approximation based on values from plots for gamma (=e) and protons (=pi) # g/cm2
-    Xmax_height2, Xmax_distance2 = _dist_decay_Xmax(zen2, injh2, Xmax_primary2)
+    Xmax_height2, Xmax_distance2 = _dist_decay_Xmax(zen2, altitude, Xmax_primary2) ## actually altitude counts not injection height
     #hx = h+Xmax_hor2*np.tan(0.5*np.pi-zen2)#   % Height at target shower Xmax 
     hx = h+Xmax_distance2*np.sin(0.5*np.pi-zen2)#   % Height at target shower Xmax 
 
@@ -268,7 +268,7 @@ def _scalingpulse(dist1, E1, az1, zen1, injh1, E2, az2, zen2, injh2, primary, ph
     ### the new/target position vector of the star shape plane
     Xmax_primary = _getXmax(primary, E2, zen2)# approximation based on values from plots for gamma (=e) and protons (=pi) # g/cm2
     #print("xmax value " , Xmax_primary)
-    Xmax_height, Xmax_distance = _dist_decay_Xmax(zen2, injh2, Xmax_primary)# 8000.# d_prime: distance from decay point to Xmax
+    Xmax_height, Xmax_distance = _dist_decay_Xmax(zen2, altitude, Xmax_primary)# 8000.# d_prime: distance from decay point to Xmax, altitude counts here, not injection height
     decay=np.array([0.,0.,injh2]) # ==: decay position as defined in zhaires sim, from DANTOn files
 
     # new position vector: